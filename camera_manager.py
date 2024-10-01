270bc32264aac29d39234fb360bb97e9d085dfa5
daniyal-ahmad-khan <22100215@lums.edu.pk>
2024-10-01 20:53:54 +0500

    test7

--- camera_manager.py
+++ camera_manager.py
@@ -95,7 +95,7 @@ class CameraManager(QObject):
         worker = CameraWorker(camera_index)
         worker.frame_captured.connect(self.handle_frame)
         self.workers[camera_index] = worker
-        print("camera_indexes: ", self.worksers.keys())
+        print("camera_indexes: ", self.workers.keys())
         worker.start()
         logging.info(f"Started CameraWorker for camera {camera_index}.")
 
